6b6a03ffe0e8f12c65bffc52315d0aa34b5d76ec
David R Andersen <k0rx@rxcomm.net>
2021-03-22 20:08:53 -0500

    added demodulator stats

--- goestoolsmon.py
+++ goestoolsmon.py
@@ -10,14 +10,13 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 from datetime import datetime
 
-global viterbi_last_sec, reed_solomon_last_sec, skipped_symbols_last_sec
-
 app = dash.Dash()
-app.layout = html.Div([dcc.Graph(id='goestools-gauges'),
-                       dcc.Interval(id='interval-component',interval=1*1000,n_intervals=0),])
+app.layout = html.Div([dcc.Graph(id='goestools-decoder-gauges'),
+                       dcc.Graph(id='goestools-demodulator-gauges'),
+                       dcc.Interval(id='interval-component',interval=1*1000,n_intervals=0)])
 
-def update_stats():
-    global viterbi_last_sec, reed_solomon_last_sec, skipped_symbols_last_sec
+def update_decoder():
+    global viterbi_last_sec, reed_solomon_last_sec, skipped_symbols_last_sec, gain_last_sec, frequency_last_sec, omega_last_sec
 
     sub = nnpy.Socket(nnpy.AF_SP, nnpy.SUB)
     sub.connect('tcp://192.168.1.109:6002')
@@ -39,7 +38,7 @@ def update_stats():
             viterbi_last_sec = int(viterbi_errors/num)
             reed_solomon_last_sec = int(reed_solomon_errors/num)
             skipped_symbols_last_sec = int(skipped_symbols/num)
-            print(num, viterbi_last_sec, reed_solomon_last_sec, skipped_symbols_last_sec)
+            #print(num, viterbi_last_sec, reed_solomon_last_sec, skipped_symbols_last_sec)
             viterbi_errors = 0
             reed_solomon_errors = 0
             skipped_symbols = 0
@@ -50,9 +49,43 @@ def update_stats():
         skipped_symbols += data['skipped_symbols']
         num += 1
 
-@app.callback(Output('goestools-gauges','figure'),Input('interval-component','n_intervals'))
-def update_viterbi(n):
-    global viterbi_last_sec, reed_solomon_last_sec, skipped_symbols_last_sec
+def update_demodulator():
+    global viterbi_last_sec, reed_solomon_last_sec, skipped_symbols_last_sec, gain_last_sec, frequency_last_sec, omega_last_sec
+
+    sub = nnpy.Socket(nnpy.AF_SP, nnpy.SUB)
+    sub.connect('tcp://192.168.1.109:6001')
+    sub.setsockopt(nnpy.SUB, nnpy.SUB_SUBSCRIBE, '')
+
+    gain_last_sec = 0
+    frequency_last_sec = 0
+    omega_last_sec = 0
+    gain = 0
+    frequency = 0
+    omega = 0
+    num = 1
+    timeold = 0
+
+    while True:
+        data = json.loads(sub.recv())
+        time=datetime.fromisoformat(data['timestamp'][:-1]).second
+        if time != timeold:
+            gain_last_sec = int(gain/num)
+            frequency_last_sec = int(frequency/num)
+            omega_last_sec = int(omega/num)
+            #print(num, gain_last_sec, frequency_last_sec, omega_last_sec)
+            gain = 0
+            frequency = 0
+            omega = 0
+            num = 0
+            timeold = time
+        gain += data['gain']
+        frequency += data['frequency']
+        omega += data['omega']
+        num += 1
+
+@app.callback(Output('goestools-decoder-gauges','figure'),Input('interval-component','n_intervals'))
+def update_decoder_plot(n):
+    global viterbi_last_sec, reed_solomon_last_sec, skipped_symbols_last_sec, gain_last_sec, frequency_last_sec, omega_last_sec
 
     fig = make_subplots(
         rows=1, cols=3,
@@ -96,9 +129,55 @@ def update_viterbi(n):
         title = {'text': "Skipped Symbols"}),
         row=1,col=3)
 
-    fig.update_layout(height=500, width=1000)
+    fig.update_layout(height=325, width=1000)
+    return fig
+
+@app.callback(Output('goestools-demodulator-gauges','figure'),Input('interval-component','n_intervals'))
+def update_demodulator_plot(n):
+    global viterbi_last_sec, reed_solomon_last_sec, skipped_symbols_last_sec, gain_last_sec, frequency_last_sec, omega_last_sec
+
+    fig = make_subplots(
+        rows=1, cols=3,
+            column_widths=[0.3, 0.3, 0.3],
+            row_heights=[1.0,],
+            specs=[[{"type": "indicator"}, {"type": "indicator"}, {"type": "indicator"},],])
+
+    fig.add_trace(go.Indicator(
+        mode = "gauge+number",
+        value = gain_last_sec,
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        gauge = {'axis': {'range': [None, 50]}},
+        title = {'text': "Gain"}),
+        row=1,col=1)
+
+    if frequency_last_sec > 10:
+        scale=100
+    else:
+        scale=10
+    fig.add_trace(go.Indicator(
+        mode = "gauge+number",
+        value = frequency_last_sec,
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        gauge = {'axis': {'range': [None, -3000]}},
+        title = {'text': "Frequency"}),
+        row=1,col=2)
+
+    if omega_last_sec > 10:
+        scale=60
+    else:
+        scale=10
+    fig.add_trace(go.Indicator(
+        mode = "gauge+number",
+        value = omega_last_sec,
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        gauge = {'axis': {'range': [None, 5]}},
+        title = {'text': "Omega"}),
+        row=1,col=3)
+
+    fig.update_layout(height=325, width=1000)
     return fig
 
 if __name__ == "__main__":
-    thread = threading.Thread(target=update_stats).start()
+    thread = threading.Thread(target=update_decoder).start()
+    thread2 = threading.Thread(target=update_demodulator).start()
     app.run_server(debug=False, use_reloader=False)
